6248175f0cb4ee385f247da804dbf996b1e5fede
Eduardo Bertassi <Eduardo@NotebookHP>
2016-05-31 09:33:43 -0300

    Correction of the Server and Team files.

--- src/util/StartTeams.java
+++ src/util/StartTeams.java
@@ -4,6 +4,9 @@ import jacamo.infra.JaCaMoLauncher;
 import jason.JasonException;
 import massim.javaagents.App;
 
+/*
+ * 
+ */
 public class StartTeams {
 
 	public static void main(String[] args) throws JasonException {
@@ -13,11 +16,11 @@ public class StartTeams {
 			@Override
 			public void run() {
 				// SimpleDebugStreamAgent.actionconfformat = "conf/team-b/actionconf/%1$s-%2$d";
-				App.main(new String[] { "./conf/team-b/javaagentsconfig.xml" });
+				App.main(new String[] { "./conf/team-g1/javaagentsconfig.xml" });
 			}
 		}).start();
 		
-		System.out.println("Java agents started.");
+		System.out.println("G1 Java Agents Started...");
 
 		// starts the JaCaMo team
 		JaCaMoLauncher.main(new String [] { "scenario1.jcm" } );
